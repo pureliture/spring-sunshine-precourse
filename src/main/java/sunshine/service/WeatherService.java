7153195d915ca2a651e5642f0eb85b4c31ef397d
이동환/광고플랫폼개발팀 <ldh2364@lguplus.co.kr>
2025-12-10 14:42:16 +0900

    refactor: remove WeatherMapper

--- src/main/java/sunshine/service/WeatherService.java
+++ src/main/java/sunshine/service/WeatherService.java
@@ -1,17 +1,13 @@
 package sunshine.service;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
-import sunshine.dto.CoordinatesResponse;
 import sunshine.enums.WmoCode;
 import sunshine.feign.client.CoordinatesClient;
 import sunshine.feign.client.WeatherClient;
 import sunshine.feign.dto.FeignCoordinatesResponse;
 import sunshine.feign.dto.FeignWeatherResponse;
-import sunshine.mapper.WeatherMapper;
 
 @Service
 @RequiredArgsConstructor
@@ -19,17 +15,14 @@ public class WeatherService {
 
 	private final CoordinatesClient coordinatesClient;
 	private final WeatherClient weatherClient;
-	private final WeatherMapper weatherMapper;
 
 	public String getWeather(String city) {
 		FeignCoordinatesResponse coordinates = coordinatesClient.getCoordinates(city).getFirst();
-		CoordinatesResponse coordinatesResponse = weatherMapper.toCoordinatesResponse(coordinates);
-
-		FeignWeatherResponse currentWeather = weatherClient.getWeather(coordinatesResponse.lat(), coordinatesResponse.lon(),
+		FeignWeatherResponse currentWeather = weatherClient.getWeather(coordinates.lat(), coordinates.lon(),
 			"temperature_2m,relative_humidity_2m,apparent_temperature,weather_code");
 
 		return String.format("%s의 현재 온도는 %s%s, 체감온도는 %s%s, 하늘 상태는 %s, 습도는 %s%s 입니다."
-			,coordinatesResponse.name()
+			,coordinates.name()
 			,currentWeather.current().temperature_2m(), currentWeather.current_units().temperature_2m()
 			,currentWeather.current().apparent_temperature(), currentWeather.current_units().apparent_temperature()
 			,WmoCode.getDescription(currentWeather.current().weather_code())
