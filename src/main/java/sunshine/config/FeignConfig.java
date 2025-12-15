7351d1fbea3d13f530f1ec9d24d3bde6bc0efde6
이동환/광고플랫폼개발팀 <ldh2364@lguplus.co.kr>
2025-12-15 11:49:51 +0900

    feat: feign timeout 설정

--- src/main/java/sunshine/config/FeignConfig.java
+++ src/main/java/sunshine/config/FeignConfig.java
@@ -1,9 +1,22 @@
 package sunshine.config;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import feign.Request;
 
 @Configuration
 @EnableFeignClients(basePackages = "sunshine.feign.client")
 public class FeignConfig {
+
+	@Bean
+	public Request.Options feignRequestOptions() {
+		return new Request.Options(
+			5, TimeUnit.SECONDS, 5, TimeUnit.SECONDS, true
+		);
+	}
+
 }
